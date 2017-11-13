2d8e96c070782e102ca4737101cc357a45d2070a
Owen Lloyd <owen7lloyd@gmail.com>
2017-11-13 21:22:39 +0000

    Customized

--- that_was_easy/main.js
+++ that_was_easy/main.js
@@ -1,5 +1,5 @@
 function sayThatWasEasy(){
-var thatWasEasy = new Audio("that_was_easy.mp3");
+var thatWasEasy = new Audio("You_Are_a_Pirate!.mp3");
 thatWasEasy.play();
 }
 $("#easy").on("click", sayThatWasEasy);
